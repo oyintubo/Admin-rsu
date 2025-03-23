9b053ebf8cbb2b4b1abea9e8d34edb1e26e79611
oyintubo <oyintuboki@gmail.com>
2025-03-24 00:41:45 +0100

    Adding admin-rsublog projexts

---
+++ index.js
@@ -0,0 +1,18 @@
+const menuLinks = document.querySelectorAll('.menu-link');
+
+function handleClick(event) {
+    menuLinks.forEach(link => {
+        link.classList.remove('bg-success', 'text-white');
+        link.classList.add('text-success'); 
+    });
+
+ 
+    const clickedLink = event.currentTarget;
+    clickedLink.classList.add('bg-success', 'text-white');
+    clickedLink.classList.remove('text-success');
+}
+
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', handleClick);
+});
